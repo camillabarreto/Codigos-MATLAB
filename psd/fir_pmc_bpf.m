c8ad6d633bcbfc76fbff39373073e5f155fba390
camillabarreto <42206341+camillabarreto@users.noreply.github.com>
2021-04-18 22:13:36 -0300

    Create fir_pmc_bpf.m

---
+++ psd/fir_pmc_bpf.m
@@ -0,0 +1,24 @@
+close all; clear all; clc;
+% Projetar o filtro passa faixa : Parks-McClellan
+
+fa = 8000;
+
+Ap = 3;          
+Ar = 40;
+Go=-10;
+       
+fp = [800 1000];
+fr = [400 1400];
+
+f = [fr(1) fp(1) fp(2) fr(2)];    
+a = [0 1 0];        
+%Apa = Ap-2; Ara = Ar+1; %n=29
+Apa = Ap; Ara = Ar; % n=22
+dev = [10^(-Ara/20) (10^(Apa/20)-1)/(10^(Apa/20)+1)  10^(-Ara/20)]; 
+[n,fo,ao,w] = firpmord(f,a,dev,fa);
+b = firpm(n,fo,ao,w);
+b=b*10^(Go/20)
+[h,w] = freqz(b,1,1024,fa);
+plot(w, 20*log10(abs(h))); hold on;
+plot([fp(1) fp(1) fp(2) fp(2)], [-Ar -Ap/2 -Ap/2 -Ar]+Go,':m')
+plot([0 fr(1) fr(1) fr(2) fr(2) fa/2], [-Ar -Ar Ap/2 Ap/2 -Ar -Ar]+Go,':m');
